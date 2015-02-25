da543410b9084bbc874ba651190bd6e0ef103214
Bikash <er.bikash21@gmail.com>
2015-02-25 02:18:12 +0100

    Total no of male survived

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -27,6 +27,13 @@ str(df_train)
 #
 
 ## number of female survived
-female_count <- nrow(df_train %>% filter(Sex == 'female'))
-female_survived <- nrow( df_train %>% filter(Sex == 'female',Survived == 1) )
-print (sprintf("F-Survived   = %2.2f %%", 100 * (female_survived/female_count)))
+female_count <- nrow(df_train %>% filter(Sex == 'female')) ## total number of female passenger
+female_survived <- nrow( df_train %>% filter(Sex == 'female',Survived == 1) ) ## total number of female passengers survived.
+print (sprintf("Total No. of Female Survived   = %2.2f %%", 100 * (female_survived/female_count))) # percentage of female survived
+
+## number of male survived.
+male_count <- nrow(df_train %>% filter(Sex == 'male')) ## total number of male passengers
+male_survived <- nrow( df_train %>% filter(Sex == 'male',Survived == 1) ) ## total number of male survived.
+print (sprintf("Total No. of Male Survived   = %2.2f %%", 100 * (male_survived/male_count))) # percentage of male survived.
+
+
