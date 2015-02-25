bc2be003732ad15c51a754ebe20d0621bc31b578
Bikash <er.bikash21@gmail.com>
2015-02-25 03:44:54 +0100

    create prediction model based on sex.

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -24,7 +24,7 @@ summary(df_train)
 glimpse(df_train)
 #
 str(df_train)
-#
+
 
 ## number of female survived
 female_count <- nrow(df_train %>% filter(Sex == 'female')) ## total number of female passenger
@@ -36,4 +36,20 @@ male_count <- nrow(df_train %>% filter(Sex == 'male')) ## total number of male p
 male_survived <- nrow( df_train %>% filter(Sex == 'male',Survived == 1) ) ## total number of male survived.
 print (sprintf("Total No. of Male Survived   = %2.2f %%", 100 * (male_survived/male_count))) # percentage of male survived.
 
+## Total percentage of people survived.
+print (sprintf("All-Survived   = %2.2f %%", 100 * (nrow( df_train %>% filter(Survived == 1) )/nrow(df_train))))
+## Total percentage of people dead.
+print (sprintf("All-Not Survived   = %2.2f %%", 100 * (nrow( df_train %>% filter(Survived == 0) )/nrow(df_train))))
 
+## run test for check sex= male and predict survived.
+head(df_test)
+df_test$Survived <- ifelse(df_test$Sex == "male",1,0) ## if male survived =1 else female survived =0
+head(df_test)
+out <- df_test%>% select(PassengerId,Survived)
+write.csv(out,"data-cleanup/r-sub-01.csv",row.names = FALSE)
+
+## run test for check sex=female
+df_test$Survived <- ifelse(df_test$Sex == "female",1,0)
+head(df_test)
+out <- df_test %>% select(PassengerId,Survived)
+write.csv(out,"data-cleanup/r-sub-02.csv",row.names = FALSE)
