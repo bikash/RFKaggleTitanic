ab26ff1a5c815069672de0768383ba63c9b72fcd
Bikash <er.bikash21@gmail.com>
2015-02-25 04:11:48 +0100

    Random forest prediction output

--- R/RandomForest.R
+++ R/RandomForest.R
@@ -18,3 +18,33 @@ head(df_train)
 summary(df_train)
 glimpse(df_train)
 str(df_train)
+
+########Simple tree model######################################
+head(df_train)
+df_train_1 <- df_train %>% select(-Name,-Ticket,-Cabin,-Embarked,-Age)
+head(df_train_1)
+
+######## transform the test data##############################
+#############################################################
+df_test_1 <- df_test %>% select(-Name,-Ticket,-Cabin,-Embarked,-Age)
+df_test_1$Survived <- NULL # Delete the column if we have survived
+str(df_test_1)
+#
+###############################################################
+### Random Forest #############################################
+###############################################################
+library(randomForest)
+mdl_rf <- randomForest(Survived ~ Pclass + Sex + SibSp+Parch+Fare, data = df_train_1,importance = TRUE)
+mdl_rf
+importance(mdl_rf)
+summary(df_test_1)
+df_test_1$Fare[is.na(df_test_1$Fare)] <- 35.0
+
+## prediction
+pred <- predict(mdl_rf,newdata = (df_test_1 %>% select(-PassengerId)))
+pred
+df_test_1$Survived <- pred
+head(df_test_1)
+out <- df_test_1 %>% select(PassengerId,Survived)
+write.csv(out,"data-cleanup/r-prediction-rf.csv",row.names = FALSE)
+#
